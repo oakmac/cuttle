6d48a15a1a4377ff210ccb022a9897d71fc8c9e1
Chris Oakman <chris@oakmac.com>
2014-10-15 14:57:14 -0500

    fix last commit

--- src-cljs/cljsbuild_ui/exec.cljs
+++ src-cljs/cljsbuild_ui/exec.cljs
@@ -104,7 +104,7 @@
 ;; Helper
 ;;------------------------------------------------------------------------------
 
-(def on-windows? (.test js/process.platform #"^win"))
+(def on-windows? (.test #"^win" js/process.platform))
 
 (defn- project-file->cwd [f]
   (replace f #"project\.clj$" ""))
