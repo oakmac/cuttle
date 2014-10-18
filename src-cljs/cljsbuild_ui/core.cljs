2053495615f8bc97e40df66c7683e9e94051f81e
Chris Oakman <chris@oakmac.com>
2014-10-17 22:12:32 -0500

    parse project.clj file

--- src-cljs/cljsbuild_ui/core.cljs
+++ src-cljs/cljsbuild_ui/core.cljs
@@ -1,7 +1,7 @@
 (ns cljsbuild-ui.core
   (:require
     [cljs.reader :refer [read-string]]
-    [clojure.string :refer [join replace split]]
+    [clojure.string :refer [join replace split trim]]
     [cljsbuild-ui.pages.main]
     [cljsbuild-ui.util :refer [log js-log on-windows? uuid]]))
 
@@ -20,38 +20,31 @@
     (.stringify js/JSON (array) nil 2)
     (js-obj "encoding" "utf8")))
 
-;; can't tell if this hack is horrible or brilliant
-(defn- defproject->map [s1]
-  (let [arr (split s1 #"\s")
-        prj-name (nth arr 1)
-        version (nth arr 2)
-        s2 (str "{" (join " " (drop 3 arr)))]
-    ; (js-log s2)
-    ; (js-log "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
-    ; (js-log (replace #";.*\n"))
-    (-> s2
-      (replace #";.+\n" "")
-      (replace #"\)$" "}")
-      (replace "#(" "(") ;; remove tagged literals
-      )))
+;; TODO: we probably need to add the name and version to our project map here
+(defn- parse-project-file [s1]
+  (let [s2 (replace s1 "#(" "(") ;; prevent "Could not find tag parser for" error
+        prj1 (read-string s2)]
+    (apply hash-map (drop 3 prj1))))
 
-;; TODO: need to handle files that do not exist, can punt on this for now
-;; TODO: parse the project.clj using transit here
-;; TODO: need to do some quick validation of the project.clj file
+;; TODO:
+;; - Need to handle files listed in projects.json that no longer exist on disk
+;;   (ie: did you remove this file?)
+;; - need to do some quick validation of project.clj
+;;   (ie: does it have :cljsbuild?)
 (defn- load-project-file [prj-file]
-  (let [file-contents (.readFileSync fs prj-file)
-        ;;prj (read-string (defproject->map file-contents))
-        prj (defproject->map file-contents)
-        ]
-    prj))
+  (let [file-contents (.readFileSync fs prj-file (js-obj "encoding" "utf8"))]
+    (parse-project-file file-contents)))
 
+;; TODO: need to do some quick validation on projects.json format here
 (defn- receive-app-data-path [app-data-path]
   (let [projects-file (str app-data-path slash "projects.json")]
     (when-not (.existsSync fs projects-file)
       (create-default-projects-file! app-data-path projects-file))
     (let [prj-files (js->clj (js/require projects-file))
           projects (zipmap prj-files (map load-project-file prj-files))]
-      (log projects))))
+      ;; it works!
+      (log (:cljsbuild (first (vals projects))))
+      )))
 
 ;; receive the OS-normalized app data path from app.js
 ;; NOTE: this event is effectively "global app init" for the webpage
