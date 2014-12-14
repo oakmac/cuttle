f2339203c10d645e3b410c1f5d3096fb459dad6d
Shaun Williams <shaunewilliams@gmail.com>
2014-12-14 10:53:40 -0600

    config-deps: upgrade rewrite-clj for bug-fix

--- app/bin/config-deps/project.clj
+++ app/bin/config-deps/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]
-                 [rewrite-clj "0.3.11"]
+                 [rewrite-clj "0.3.12"]
                  [leiningen-core "2.5.0"]
                  [me.raynes/fs "1.4.6"]]
   :main config-deps.core)
