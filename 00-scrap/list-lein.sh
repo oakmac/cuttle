9c6fc1bd1350b70341c885863de869442c94a31d
Shaun Williams <shaunewilliams@gmail.com>
2015-01-18 16:41:11 -0600

    fix kill-lein and add list-lein utility script for manually monitoring lein processes kicked off by our app

--- 00-scrap/list-lein.sh
+++ 00-scrap/list-lein.sh
@@ -3,4 +3,4 @@
 # This kills all cljsbuild-ui leiningen processes
 # We need this on Mac currently since "stop" is not killing them yet.
 
-ps -ef | grep -v grep | grep "with-profile +cljsbuild-ui" | cut -d" " -f4 | xargs kill
+ps -ef | grep -v grep | grep "with-profile +cljsbuild-ui"
