0a07cc84beb99f7be4351714d4f685bfa792b15b
Shaun Williams <shaunewilliams@gmail.com>
2015-01-26 23:18:30 -0800

    fix windows lein-profile script

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -21,7 +21,7 @@ echo. & echo Installing Atom Shell...
 call grunt download-atom-shell
 
 echo. & echo Building lein profile tool...
-call build-lein-profile-tool.bat
+call "scripts\build-lein-profile-tool.bat"
 
 echo. & echo Cuttle setup complete.
 
