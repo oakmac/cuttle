646fadeee6b2aa3086da0175c1bad448276c4931
Shaun Williams <shaunewilliams@gmail.com>
2015-03-20 23:17:10 -0500

    prevent installing grunt-appdmg on linux/win
    created issue here:
    https://github.com/rakuten-frontend/grunt-appdmg/issues/2

--- scripts/setup.bat
+++ scripts/setup.bat
@@ -3,6 +3,9 @@
 pushd "%~dp0\.."
 
 echo. & echo Installing node dependencies...
+rem "grunt-appdmg" is a mac-only dependency that will fail to build on windows.
+rem     So we are including it as an optionalDependency in package.json
+rem     and preventing its installation with npm's --no-optional flag.
 call npm install
 
 echo. & echo Installing grunt...
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -6,7 +6,15 @@ set -e
 cd "`dirname $0`/.."
 
 echo; echo "Installing node dependencies..."
-npm install
+
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  npm install
+else
+  # "grunt-appdmg" is a mac-only dependency that will fail to build on linux.
+  # So we are including it as an optionalDependency in package.json
+  # and preventing its installation with npm's --no-optional flag.
+  npm install --no-optional
+fi
 
 echo; echo "Installing grunt..."
 sudo npm install -g grunt-cli
