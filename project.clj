318e015cea0e952c2194de3d8b02a9f3bc49c216
Chris Oakman <chris@oakmac.com>
2016-02-09 01:54:03 -0600

    parinfer formatting

--- project.clj
+++ project.clj
@@ -1,39 +1,34 @@
 (defproject cuttle "1.1"
+
   :description "A user interface for the ClojureScript compiler."
   :url "https://github.com/oakmac/cuttle"
-  :license {
-    :name "MIT License"
-    :url "https://github.com/oakmac/cuttle/blob/master/LICENSE.md"
-    :distribution :repo}
+  :license {:name "MIT License"
+            :url "https://github.com/oakmac/cuttle/blob/master/LICENSE.md"
+            :distribution :repo}
 
   :source-paths ["src-clj"]
 
-  :dependencies [
-    [org.clojure/clojure "1.6.0"]
-    [org.clojure/clojurescript "0.0-2760"]
-    [org.clojure/core.async "0.1.346.0-17112a-alpha"]
-    [hiccups "0.3.0"]
-    [quiescent "0.1.4"]
-    [sablono "0.2.22"]]
+  :dependencies
+    [[org.clojure/clojure "1.6.0"]
+     [org.clojure/clojurescript "0.0-2760"]
+     [org.clojure/core.async "0.1.346.0-17112a-alpha"]
+     [hiccups "0.3.0"]
+     [quiescent "0.1.4"]
+     [sablono "0.2.22"]]
 
-  :plugins [
-    [lein-cljsbuild "1.0.3"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
 
-  :cljsbuild {
-    :builds {
-
-      :main {
+  :cljsbuild
+    {:builds
+      [{:id "main"
         :source-paths ["src-cljs"]
-        :compiler {
-          :optimizations :whitespace
-          :output-to "app/js/main.js"}}
+        :compiler {:optimizations :whitespace
+                   :output-to "app/js/main.js"}}
 
-      ; :main-min {
-      ;   :source-paths ["src-cljs"]
-      ;   :compiler {
-      ;     :externs ["externs/react-0.11.0.js" "externs/misc.js"]
-      ;     :output-to "app/js/main.min.js"
-      ;     :optimizations :advanced
-      ;     :pretty-print false }}
-
-}})
+       {:id "main-min"
+        :source-paths ["src-cljs"]
+        :compiler
+          {:externs ["externs/react-0.11.0.js" "externs/misc.js"]
+           :output-to "app/js/main.min.js"
+           :optimizations :advanced
+           :pretty-print false}}]})
